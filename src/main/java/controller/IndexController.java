8f54d7f96945a105100393dc75c26962a27636e2
Jun <tti3366@naver.com>
2023-04-04 17:27:33 +0900

    [YSJ] update home.jsp

--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -2,28 +2,34 @@ package controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import model.User;
 import service.ClubService;
 
 @Controller
 public class IndexController {
-  
-  @Autowired
-  private ClubService clubService;
-  
-  @RequestMapping("/home")
-  public ModelAndView getClubList() {
-	System.out.println("컨트롤러 호출 성공");
-    List<String> clubSW = clubService.getClubNamesByNum(1);//동아리id가 1로 시작하는 동아리 -> 추후에 실제 학과번호에 맞춰서 수정해야함
-    ModelAndView mav = new ModelAndView();
-    mav.addObject("clubSW", clubSW);
-    mav.setViewName("home");
-    return mav;
-  }
 
+	@Autowired
+	private ClubService clubService;
+
+	@RequestMapping("/home")
+	public ModelAndView getClubList(HttpServletRequest request) {
+		ModelAndView mav = new ModelAndView();
+		
+		User userInfo = (User)request.getSession().getAttribute("SESSION");
+		
+		List<String> clubSW = clubService.getClubNamesByNum(1);//동아리id가 1로 시작하는 동아리 -> 추후에 실제 학과번호에 맞춰서 수정해야함
+		
+		mav.addObject("userInfo", userInfo);
+		mav.addObject("clubSW", clubSW);
+		mav.setViewName("home");
+		
+		return mav;
+	}
 }
-
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -60,14 +60,14 @@
       <ul class="d-flex align-items-center">
         <li class="nav-item dropdown pe-3">
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <span class="d-none d-md-block dropdown-toggle ps-2">UserName</span>
+            <span class="d-none d-md-block dropdown-toggle ps-2">${userInfo.userName}</span>
           </a>
 
 		<!-- 유저 정보  -->
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
             <li class="dropdown-header">
-              <h6>UserName</h6>
-              <span>MemberId</span>
+              <h6>${userInfo.userName}</h6>
+              <span>${userInfo.userId}</span>
             </li>
             <li>
               <hr class="dropdown-divider">
